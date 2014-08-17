bb93b5bde45f78ce0d929e0191a058813ffad4c0
Javi Manzano <javi.manzano.oller@gmail.com>
2014-08-17 21:54:41 +0100

    Add buttons to navigate between months

--- src/public/views.py
+++ src/public/views.py
@@ -1,3 +1,4 @@
+import calendar
 import datetime
 
 from django.conf import settings
@@ -31,6 +32,28 @@ class Home(PublicMixin, TemplateView):
 class ActivityList(PublicMixin, ListView):
     model = Activity
     template_name = 'public/activity_list.html'
+
+    date = None
+
+    def get_queryset(self):
+        queryset, self.date = get_activities(self.request, self.model)
+        return queryset
+
+    def get_context_data(self, *args, **kwargs):
+        ctx = super(ActivityList, self).get_context_data(*args, **kwargs)
+        next_date = self.date + datetime.timedelta(days=32)
+        previous_date = self.date - datetime.timedelta(days=self.date.day + 1)
+
+        next_month = None
+        if next_date < datetime.datetime.now():
+            next_month = '%s-%s' % (next_date.year, next_date.month)
+
+        ctx.update(dict(
+            month=calendar.month_name[self.date.month],
+            next_month=next_month,
+            previous_month='%s-%s' % (previous_date.year, previous_date.month),
+        ))
+        return ctx
 
 activity_list = ActivityList.as_view()
 
@@ -77,9 +100,24 @@ def generate_dummy_activities(request):
         return redirect('public:home')
 
 
+def get_activities(request, model):
+    if not 'date' in request.GET:
+        current_date = datetime.datetime.now()
+        min_date = datetime.date(current_date.year, current_date.month, 1)
+        queryset = model.objects.filter(date__gte=min_date)
+    else:
+        # TODO: make this nicer
+        date_str = request.GET.get('date')
+        current_date = datetime.datetime.strptime(date_str, "%Y-%m")
+        min_date = datetime.date(current_date.year, current_date.month, 1)
+        next_month = min_date + datetime.timedelta(days=32)
+        max_date = datetime.date(next_month.year, next_month.month, 1) - datetime.timedelta(days=1)
+        queryset = model.objects.filter(date__gte=min_date, date__lte=max_date)
+    return queryset, current_date
+
+
 def activity_json(request):
-    current_date = datetime.datetime.now()
-    date = datetime.date(current_date.year, current_date.month, 1)
+    queryset, date = get_activities(request, Activity)
     response_data = serializers.serialize(
-        'json', Activity.objects.filter(date__gte=date).order_by('date'))
+        'json', queryset.order_by('date'))
     return HttpResponse(response_data, content_type="application/json")
