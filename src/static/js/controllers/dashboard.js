9684311772bb0619998b725fb3b950a85000877d
Javi Manzano <javi.manzano.oller@gmail.com>
2014-09-14 18:42:38 +0100

    Add month navigation to dashboard graphs

--- src/static/js/controllers/dashboard.js
+++ src/static/js/controllers/dashboard.js
@@ -1,43 +1,57 @@
 kr.app.controller('Dashboard', ['$scope', '$log', '$modal', '$location', 'ActivityFactory', function($scope, $log, $modal, $location, ActivityFactory) {
     $log.info("Dashboard Ctrl");
 
-    $scope.stats = ActivityFactory.Stats.query();
-
-    $scope.months = [
-        {value: 0, label: 'January'},
-        {value: 1, label: 'February'},
-        {value: 2, label: 'March'},
-        {value: 3, label: 'April'},
-        {value: 4, label: 'May'},
-        {value: 5, label: 'June'},
-        {value: 6, label: 'July'},
-        {value: 7, label: 'August'},
-        {value: 8, label: 'September'},
-        {value: 9, label: 'October'},
-        {value: 10, label: 'November'},
-        {value: 11, label: 'December'}
-    ];
-
-    var now = new Date();
-    $scope.currentMonth = $scope.months[now.getMonth()];
-
-    $scope.$watch('currentMonth', function(newVal) {
-        now.setMonth(newVal.value);
-        _fetchActivities();
-    }, true);
-
-    $scope.activities = [];
-
     var _fetchActivities = function() {
         ActivityFactory.ActivitiesResource.query(
             {
-                date: now.getFullYear() + '-' + (now.getMonth() + 1)
+                date: $scope.currentYear + '-' + ($scope.currentMonth + 1)
             },
             function(activities) {
                 $scope.activities = new ActivityFactory.ActivityUtils().createActivities(activities);
             }
         );
     };
-    _fetchActivities();
 
+    $scope.stats = ActivityFactory.Stats.query();
+    $scope.activities = [];
+
+    $scope.months = [
+        'January',
+        'February',
+        'March',
+        'April',
+        'May',
+        'June',
+        'July',
+        'August',
+        'September',
+        'October',
+        'November',
+        'December'
+    ];
+
+    $scope.currentMonth = new Date().getMonth();
+    $scope.currentYear = new Date().getFullYear();
+
+    $scope.nextMonth = function() {
+        var newMonth = $scope.currentMonth + 1;
+        if (newMonth > 11) {
+            newMonth = 11;
+            $scope.currentYear = $scope.currentYear + 1;
+        }
+        $scope.currentMonth = newMonth;
+    };
+
+    $scope.previousMonth = function() {
+        var newMonth = $scope.currentMonth - 1;
+        if (newMonth < 0) {
+            newMonth = 11;
+            $scope.currentYear = $scope.currentYear - 1;
+        }
+        $scope.currentMonth = newMonth;
+    };
+
+    $scope.$watch('currentMonth', function(newVal) {
+        _fetchActivities();
+    }, true);
 }]);
