85ff2ab88974c14bd16fa5ace322ca40cb5398f0
Javi Manzano <javi.manzano.oller@gmail.com>
2014-09-19 18:09:17 +0200

    Remove unused templatetag

--- src/public/templatetags/format.py
+++ src/public/templatetags/format.py
@@ -3,14 +3,6 @@ from django import template
 register = template.Library()
 
 
-@register.filter(name='format_time')
-def format_time(value):
-    if value.hour > 0:
-        return "%s:%s:%s" % (value.hour, value.minute, value.second)
-    else:
-        return "%s:%s" % (value.minute, value.second)
-
-
 @register.filter(name='get_ng_model')
 def get_ng_model(attrs):
     return attrs.get('ng-model', '')
